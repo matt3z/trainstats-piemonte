126a41f00748cca698820c958abe6dcb46bbded6
matt3z <43523121+matt3z@users.noreply.github.com>
2025-01-03 16:23:56 +0100

    Update informazioni.py
    Updated site guide informations

--- sections/informazioni.py
+++ sections/informazioni.py
@@ -10,10 +10,11 @@ st.set_page_config(
 
 st.title("Informazioni sul sito")
 
-st.subheader('Come usare il sito:')
+st.subheader("Per l'utilizzo del sito:")
 st.markdown("Nella homepage sono presenti due schede: **'Statistiche e grafici'** e **'Mappa'**:")
-st.markdown("Nella scheda **'Statistiche e grafici'**, selezionare una delle linee disponibili dal menù a tendina. Verranno visualizzate le statistiche generali relative all'ultima settimana e dei grafici per i quali è selezionabile l'intervallo di date attraverso l'apposito menù.")
-st.markdown("Nella scheda **'Mappa'** è presente una mappa con evidenziate le stazioni e le linee. È possibile selezionare se visualizzare i dati relativi alle linee oppure alle stazioni al passaggio del mouse, attraverso il selettore presente sopra alla cartina. La media dei ritardi, per quanto riguarda le stazioni, si riferisce ai ritardi dei treni in arrivo. Anche il ritardo medio di ogni linea è calcolato in base all'orario di arrivo a destinazione del treno.")
+st.markdown("- Nella scheda **'Statistiche e grafici'**, selezionare una delle linee disponibili dal menù a tendina. Verranno visualizzate le statistiche generali relative all'ultima settimana e dei grafici per i quali è selezionabile l'intervallo di date attraverso l'apposito menù.\n- Nella scheda **'Mappa'** è presente una mappa con evidenziate le stazioni. È possibile visualizzare i dati del ritardo medio dei treni che terminano la corsa nelle relative stazioni, al passaggio del mouse sull'indicatore.")
+st.markdown("Nel portale sono inoltre presenti le sezioni **'Dashboard SFM'** e **'Guasti e disservizi'**, raggiungibili attraverso la barra laterale:")
+st.markdown("- Nella sezione **'Dashboard SFM'**, sono presenti statistiche e grafici relativi all'intero Servizio Ferroviario Metropolitano, per permetterne un monitoraggio semplice e immediato.\n- Nella sezione **'Guasti e disservizi'**, è presente una banca dati costantemente aggiornata sui guasti all'infrastruttura ferroviaria piemontese.")
 
 st.subheader('Info:')
 st.info("Webapp sviluppata in Python grazie a Streamlit.\n\nI dati su cui sono elaborate le statistiche sono provenienti da Viaggiatreno.\n\nPer contatti: info@ferroviebiellesi.it\n\n© Matteo Manfrin - 2024")
